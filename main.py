eb98e02f5845841036b7836d09d252f7a1a2d599
AJ <ajaybollipo8@gmail.com>
2026-09-17 19:49:52 +0530

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-print("================================")
-print("        AJ AI ASSISTANT")
-print("================================")
-
-print("AJ is starting...")
-print("Hello Ajay! I am AJ.")
-print("Your personal AI assistant.")
