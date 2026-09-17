db356d767996d124063c759c2c0f474b1162350a
AJ <ajaybollipo8@gmail.com>
2026-09-17 19:47:13 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+print("================================")
+print("        AJ AI ASSISTANT")
+print("================================")
+
+print("AJ is starting...")
+print("Hello Ajay! I am AJ.")
+print("Your personal AI assistant.")
